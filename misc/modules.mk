2e0b5ea081489e692f7b6ffcb11fc71e3c5ebd19
jossafossa <joosthobma@gmail.com>
2019-05-27 11:11:46 +0200

    modules

---
+++ misc/modules.mk
@@ -0,0 +1,7 @@
+# modules
+|-|-|
+| HomeModule | `NavModule`, `WelcomeComponent` |
+| NavModule | `LoginButtonComponent`, `LogoutButtonComponent`, `SignupButtonComponent`  | 
+| CreateAccountModule | `AuthenticationInfoComponent`, `EditBasicInfoComponent`, `EditInterestsComponent`, `EditPhotosComponent` | 
+| ViewAccountModule | `ViewBasicInfoComponent`, `EditInterestsComponent`, `EditPhotosComponent`  |
+| 
