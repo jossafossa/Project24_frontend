9f0b05bbe20a9fe801352d734f4f8e57ebe70173
HDJRijnders <hylbrenrijnders@hotmail.com>
2019-06-18 14:37:04 +0200

    Noticeboard met mock data

--- shunter/src/app/group-accounts/noticeboard/board/board.component.ts
+++ shunter/src/app/group-accounts/noticeboard/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NOTICES } from '../notice/mock-notices';
 
 @Component({
   selector: 'app-board',
@@ -6,12 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-
+notices = NOTICES;
   constructor() { }
 
   ngOnInit() {
   }
-public addnotice(){
-    return null
-}
+
 }
---
+++ shunter/src/app/group-accounts/noticeboard/notice/notice.ts
@@ -0,0 +1,7 @@
+export class Notice {
+  id: number;
+  user: string;
+  date: string;
+  title: string;
+  message: string;
+}
